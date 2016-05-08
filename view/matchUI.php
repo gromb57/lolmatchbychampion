4dd15fcc65b61cee9b6d933d7aaa40e8e0efd39e
jérôme bach <jeromeb57@gmail.com>
2016-05-08 20:02:27 +0200

    improve match ui

--- view/matchUI.php
+++ view/matchUI.php
@@ -2,10 +2,15 @@
 
 class MatchUI{
 	static function noMatch() {
-		echo "<p>".("No matches found.")."</p>";
+		echo '<div class="card blue-grey darken-1">';
+			echo '<div class="card-content white-text">';
+				echo '<span class="card-title">'.("No matches found.").'</span>';
+			echo '</div>';
+		echo '</div>';
 	}
 
 	static function render($match) {
+		// see: https://developer.riotgames.com/docs/spectating-games
 		$client_path = "C:\Riot Games\League of Legends\RADS\solutions\lol_game_client_sln\releases\0.0.1.74\deploy\League of Legends.exe";
 		$maestro = "8394 LoLLauncher.exe";
 		$params =  '"" "spectator spectator.euw1.lol.riotgames.com:80"';
@@ -14,8 +19,17 @@ class MatchUI{
 		$platformId = $match->platformId;
 
 		$cmd = "$client_path $maestro $params $encryptionKey $gameId $platformId";
-var_dump($match);
-		echo "<p>Launch the game : $cmd</p>";
+
+		echo '<div class="card blue-grey darken-1">';
+			echo '<div class="card-content white-text">';
+				echo '<span class="card-title">Game mode : '.$match->gameMode.'</span>';
+				echo '<p>';
+					echo 'Match start at : '.date("Y-m-d h:i:s", $match->gameStartTime).'<br/>';
+					echo 'Match length : '.round($match->gameLength / 60).'\'<br/>';
+					echo 'Launch the game : '.$cmd.'<br/>';
+				echo '</p>';
+			echo '</div>';
+		echo '</div>';
 	}
 }
 ?>
